557aaa03be04277c9c52930f47b8e47be0050cce
tarunreddy007 <91886378+tarunreddy007@users.noreply.github.com>
2021-12-06 15:19:02 +0530

    springboot sample

---
+++ spring-boot-hello-world-example/src/main/java/com/test/SpringBootHelloWorldExampleApplication.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootHelloWorldExampleApplication  extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+	}
+}
